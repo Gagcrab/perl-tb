ceafea8f4d44730654b09287d9943d3ad144589f
Michael Suzzi <msuz.code@gmail.com>
2022-03-04 20:16:34 +1100

    pod2markdown: Take perl file as argument instead of hardcoding

--- pod2markdown.sh
+++ pod2markdown.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env sh
-# Very simple shell script to convert perl POD to markdown for Github.
+# Very simple shell script to convert POD documentation embedded in a perl
+# script or module to a markdown README, for use with Github.
+#
+# Author: Michael Suzzi
+# Last modified: 2022-03-04
+#
+# Usage: pod2markdown.sh <name of perl script containing POD> 
+#
 echo "Converting POD to markdown..."
-if perl -MPod::Markdown -e 'Pod::Markdown->new->filter(@ARGV)' tb > README.md; then
-    echo "Success." >&2
-    exit 0
+
+if [ $# -eq 0 ]; then
+	echo "No perl file specified"
+	exit 1
+fi
+
+if perl -MPod::Markdown -e 'Pod::Markdown->new->filter(@ARGV)' "$1" > README.md; then
+    	echo "Success" >&2
+    	exit 0
 else
-    echo "Failure." >&2
-    exit 255
+	echo "Failure" >&2
+	exit 255
 fi
